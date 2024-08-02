2cda11f667c919e94920578f4ec805c72b969f82
Jaranag <joel.aranag@gmail.com>
2024-08-02 09:34:37 +0200

    initial commit

---
+++ src/main/java/com/grupo/spent/SpentApplication.java
@@ -0,0 +1,13 @@
+package com.grupo.spent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo/spent/SpentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.spent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
