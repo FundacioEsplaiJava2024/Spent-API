43188a14b3f806ecd2fc1bba7ea0f8b64db7f1a8
David Freixes <dfis1996@gmail.com>
2024-08-08 13:45:25 +0200

    new getUserByUsername

--- src/main/java/com/grupo/spent/services/UserService.java
+++ src/main/java/com/grupo/spent/services/UserService.java
@@ -6,4 +6,5 @@ public interface UserService {
     User register(String email, String username, String name, String password);
     String login(String email, String password);
     User findUserByUsername(String username);
+    User getUserByUsername(String username);
 }
