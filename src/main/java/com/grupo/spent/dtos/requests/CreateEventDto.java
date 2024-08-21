b68ea803324783177b2868c150477537dd6d15e6
David Freixes <dfis1996@gmail.com>
2024-08-21 13:59:14 +0200

    firsts exceptions

--- src/main/java/com/grupo/spent/dtos/requests/CreateEventDto.java
+++ src/main/java/com/grupo/spent/dtos/requests/CreateEventDto.java
@@ -3,16 +3,32 @@ package com.grupo.spent.dtos.requests;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateEventDto {
+
+    @NotEmpty(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
+    @Size(min = 6, max = 40, message = "Password should be 8 characters long minimum")
     private String title;
+
+    @NotEmpty(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
+    @Size(min = 10, max = 140, message = "Password should be 8 characters long minimum")
     private String description;
+
     private LocalDate date;
     private LocalTime startTime;
     private LocalTime endTime;
     private Integer numParticipants;
+
+    @NotEmpty(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
+    @Size(min = 2, max = 30, message = "Password should be 8 characters long minimum")
     private String address;
     private String sportName;
 }
--- src/main/java/com/grupo/spent/dtos/requests/RegisterDto.java
+++ src/main/java/com/grupo/spent/dtos/requests/RegisterDto.java
@@ -8,22 +8,28 @@ import lombok.Data;
 
 @Data
 public class RegisterDto {
-    // @Email(message = "Email is not well formatted")
-    // @NotEmpty(message = "Email is mandatory")
-    // @NotNull(message = "Email is mandatory")
+    @Email(message = "Email is not well formatted")
+    @NotEmpty(message = "Email is mandatory")
+    @NotNull(message = "Email is mandatory")
     private String email;
 
-    // @NotEmpty(message = "Username is mandatory")
-    // @NotNull(message = "Username is mandatory")
-    // @Size(min = 8, message = "Username should be 8 characters long minimum")
+    @NotEmpty(message = "Username is mandatory")
+    @NotNull(message = "Username is mandatory")
+    @Size(min = 5, max = 15, message = "Username should be 8 characters long minimum")
     private String username;
 
-    // @NotEmpty(message = "Password is mandatory")
-    // @NotNull(message = "Password is mandatory")
-    // @Size(min = 8, message = "Password should be 8 characters long minimum")
+    @NotEmpty(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
+    @Size(min = 5, max = 15, message = "Password should be 8 characters long minimum")
     private String password;
 
-    // @NotEmpty(message = "Name is mandatory")
-    // @NotNull(message = "Name is mandatory")
-    // @Size(min = 2, message = "Username should be 2 characters long minimum")
-    private String name;}
+    @NotEmpty(message = "Name is mandatory")
+    @NotNull(message = "Name is mandatory")
+    @Size(min = 1, max = 20, message = "Username should be 2 characters long minimum")
+    private String name;
+
+    // public User toUser() {
+    //     return new User()
+    //         .setEmail(email.toLowerCase())
+    //   }
+}
